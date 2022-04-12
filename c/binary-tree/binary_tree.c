00a7585936957b1c50831562d7bc7d0b85f7cc24
ArchTangent <95271114+ArchTangent@users.noreply.github.com>
2022-04-11 18:50:48 -0700

    Bugfix in printTree indexing

--- c/binary-tree/binary_tree.c
+++ c/binary-tree/binary_tree.c
@@ -1,6 +1,8 @@
-// Binary Tree structure in C.
+// Binary Tree structure in C for practice purposes.
+// Note: errors seem to be due to copy mechanics (passing as value)
 
 #include <stdio.h>
+#include <assert.h>
 
 // Each Child can have 0, 1, or 2 child indexes.
 struct Child {
@@ -19,6 +21,7 @@ struct BTree {
 // BTree default constructor.
 struct BTree newBTree() {
     struct BTree t1;
+    t1.count = 0;
     t1.size = 10;
     return t1;
 }
@@ -28,20 +31,41 @@ void printChild(struct Child c) {
 }
 
 void printTree(struct BTree t) {
-    printf("BTree:\n");
+    assert(t.count >= 0);
+
+    printf("BTree: ");
+    if (t.count == 0) {
+        printf("empty\n");
+        return;
+    }
+    printf("\n");
     int i;
-    for (i = 0; i < t.count; i++) {
-        printChild(t.vals[i]);
+    for (i = t.count; i > 0; i--) {
+        printf(" - ");
+        printChild(t.vals[i - 1]);
+    }
+}
+
+// Adds a Child to the BTree if not full.
+void pushChild(struct BTree t, struct Child c) {
+    printf("pushing child...\n");
+    if (t.count < t.size) {
+        t.vals[t.count + 1] = c;
+        t.count++;
+    } else {
+        printf("BTree of size %d is full!\n", t.size);
     }
 }
 
 int main()
 {
     struct Child c0 = {"1st", 0, -1, -1};
-    struct BTree t1 = newBTree();
+    struct BTree t0 = newBTree();
 
+    pushChild(t0, c0);
     printChild(c0);
-    printTree(t1);
+    printTree(t0);
+    printf("count: %d", t0.count);
 
     return 0;
 }
