649a5fa34171ebf30ebfb452768076990cbc345b
ArchTangent <95271114+ArchTangent@users.noreply.github.com>
2022-04-11 17:38:07 -0700

    Added C BTree helper functions

--- c/binary-tree/binary_tree.c
+++ c/binary-tree/binary_tree.c
@@ -16,10 +16,32 @@ struct BTree {
     int     size;
 };
 
+// BTree default constructor.
+struct BTree newBTree() {
+    struct BTree t1;
+    t1.size = 10;
+    return t1;
+}
+
+void printChild(struct Child c) {
+    printf("Child [%d] '%s': %d, %d\n", c.ix, c.name, c.child1, c.child2);
+}
+
+void printTree(struct BTree t) {
+    printf("BTree:\n");
+    int i;
+    for (i = 0; i < t.count; i++) {
+        printChild(t.vals[i]);
+    }
+}
+
 int main()
 {
     struct Child c0 = {"1st", 0, -1, -1};
-    printf("Child 1: %s %d %d, %d\n", c0.name, c0.ix, c0.child1, c0.child2);
+    struct BTree t1 = newBTree();
+
+    printChild(c0);
+    printTree(t1);
 
     return 0;
 }
