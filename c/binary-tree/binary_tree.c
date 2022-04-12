c3bbcb09fb829a1feb8841c580c8f12e1b69cb49
ArchTangent <95271114+ArchTangent@users.noreply.github.com>
2022-04-11 21:03:50 -0700

    Bugfix: changed BTree to pass by reference

--- c/binary-tree/binary_tree.c
+++ c/binary-tree/binary_tree.c
@@ -1,12 +1,13 @@
 // Binary Tree structure in C for practice purposes.
-// Note: errors seem to be due to copy mechanics (passing as value)
+// Notes:
+// - be mindful of pass by value (copy) vs by reference (pointer).
 
 #include <stdio.h>
 #include <assert.h>
 
 // Each Child can have 0, 1, or 2 child indexes.
 struct Child {
-    char name[20];
+    char name[16];
     int  ix;
     int  child1;
     int  child2;
@@ -30,30 +31,36 @@ void printChild(struct Child c) {
     printf("Child [%d] '%s': %d, %d\n", c.ix, c.name, c.child1, c.child2);
 }
 
-void printTree(struct BTree t) {
-    assert(t.count >= 0);
+void printTree(struct BTree *t) {
+    assert(t->count >= 0);
 
     printf("BTree: ");
-    if (t.count == 0) {
+    if (t->count == 0) {
         printf("empty\n");
         return;
     }
     printf("\n");
     int i;
-    for (i = t.count; i > 0; i--) {
+    for (i = t->count; i > 0; i--) {
         printf(" - ");
-        printChild(t.vals[i - 1]);
+        printChild(t->vals[i - 1]);
     }
 }
 
 // Adds a Child to the BTree if not full.
-void pushChild(struct BTree t, struct Child c) {
+void pushChild(struct BTree *t, struct Child c) {
+    printf("[pushChild] c address: %p\n", &c);
+    printf("[pushChild] t address: %p\n", &t);
+
+    printf("[pushChild] Child [%d] '%s': %d, %d\n", c.ix, c.name, c.child1, c.child2);
+
     printf("pushing child...\n");
-    if (t.count < t.size) {
-        t.vals[t.count + 1] = c;
-        t.count++;
+    if (t->count < t->size) {
+        t->vals[t->count] = c;
+        t->count++;
+        printf("[pushChild] t.count is %d\n", t->count);
     } else {
-        printf("BTree of size %d is full!\n", t.size);
+        printf("BTree of size %d is full!\n", t->size);
     }
 }
 
@@ -62,9 +69,12 @@ int main()
     struct Child c0 = {"1st", 0, -1, -1};
     struct BTree t0 = newBTree();
 
-    pushChild(t0, c0);
+    printf("[main] c0 address: %p\n", &c0);
+    printf("[main] t0 address: %p\n", &t0);
+
+    pushChild(&t0, c0);
     printChild(c0);
-    printTree(t0);
+    printTree(&t0);
     printf("count: %d", t0.count);
 
     return 0;
