a7202c7c5586d09aae30b192ce15ca40bc14f032
ArchTangent <95271114+ArchTangent@users.noreply.github.com>
2022-04-16 04:35:58 -0700

    New Python binary tree version

--- python/binary-tree/btree.py
+++ python/binary-tree/btree.py
@@ -1,13 +1,12 @@
-# Binary Tree in Python
-# TODO: add testing
+# Binary Tree in Python (1st version)
 
 class Node:
     def __init__(self, val) -> None:
         self.val = val
-        self.ix_less = None  # index of value that is <= self
-        self.ix_more = None  # index of value that is > self
+        self.left = None    # index of value that is <= self
+        self.right = None   # index of value that is > self
     def __repr__(self) -> str:
-        return f"Node {self.val}: ({self.ix_less},{self.ix_more})"
+        return f"Node {self.val}: ({self.left},{self.right})"
 
 class BTree:
     def __init__(self) -> None:
@@ -15,9 +14,7 @@ class BTree:
         self.next = 0
 
     def add_node(self, node):
-        print(f"Adding Node {node}...")
         if self.nodes.__len__() == 0:
-            print(f"Tree is empty, adding to index 0")
             self.nodes.append(node)
             self.next += 1
         else:
@@ -28,27 +25,21 @@ class BTree:
     def compare(self, n1, n2) -> bool:
         """Compares two Nodes and returns true if a valid place is found"""
         if n1.val <= n2.val:
-            print(f"n1 ({n1.val}) is <= n2 ({n2.val})")
-            if n2.ix_less is None:
-                print("n2.ix_less is None")
-                n2.ix_less = self.next
+            if n2.left is None:
+                n2.left = self.next
                 self.nodes.append(n1)
                 self.next += 1
                 return True
             else:
-                print("n2.ix_less is not None")
-                self.compare(n1, self.nodes[n2.ix_less])
+                self.compare(n1, self.nodes[n2.left])
         elif n1.val > n2.val:
-            print(f"n1 ({n1.val}) is > n2 ({n2.val})")
-            if n2.ix_more is None:
-                print("n2.ix_less is None")
-                n2.ix_more = self.next
+            if n2.right is None:
+                n2.right = self.next
                 self.nodes.append(n1)
                 self.next += 1
                 return True
             else:
-                print("n2.ix_less is not None")
-                self.compare(n1, self.nodes[n2.ix_more])
+                self.compare(n1, self.nodes[n2.right])
         return True
 
     def __repr__(self) -> str:
@@ -56,15 +47,15 @@ class BTree:
         result = []
         for node in self.nodes:
             v = node.val
-            c1 = (None if node.ix_less is None else self.nodes[node.ix_less].val)
-            c2 = (None if node.ix_more is None else self.nodes[node.ix_more].val)
+            c1 = (None if node.left is None else self.nodes[node.left].val)
+            c2 = (None if node.right is None else self.nodes[node.right].val)
             result.append(f"    Node {v}: ({c1}, {c2})")
         return '\n'.join(result)
 
 if __name__ == "__main__":
     print("--- BTREE ---")
+
     vals = [4, 8, 5, 7, 6, 2, 4, 9, 5, 1]
-    # vals = [4, 8, 5]
     tree = BTree()
     for val in vals:
         tree.add_node(Node(val))
---
+++ python/binary-tree/notes.md
@@ -0,0 +1,6 @@
+# Binary Tree Notes
+
+## Traversal Types
+- Pre-Order: from the top down, getting roots before leaves
+- Post-Order: from the bottom up, getting leaves before roots
+- In-Order: gets data according to inherent sequence (e.g alphabetical order)
