e29cfcf297837ef7dda5ed88c4115a5c016fbbea
ArchTangent <95271114+ArchTangent@users.noreply.github.com>
2022-06-05 08:38:29 -0700

    Add Breadth First Search Version 1

--- python/breadth-first-search/README.md
+++ python/breadth-first-search/README.md
@@ -2,6 +2,26 @@
 
 Breadth-First Search implementations in Python, with unit testing available via `pytest`.
 
+Edge Cases:
+- Source Node == Target Node
+- Cyclical graphs
+- Invalid Source/Target Node
+
+## Method 1: Recursive (Mutating)
+
+Big Picture:
+1. Use a `Dict(Node, List[Node])` as the graph, where all `Node`s are of the same type.
+2. Check all `connections` of `source` (starting Node) to see if they equal `target`
+    - if so, return the current `depth`
+3. If none of the `connection`s match `target`:
+    - gather `connections` in a separate list
+    - delete current `source` node from the graph (to avoid cycling)
+    - recursively perform `breadth_first_search()` for each of the missed connections
+
+Caveats:
+- this version mutates the graph in-place which may not be desired.
+- this version doesn't work if `source == target`
+
 ## Testing
 
 Ensure that `pytest` is installed, then run one of the following:
--- python/breadth-first-search/breadth_first_search_1.py
+++ python/breadth-first-search/breadth_first_search_1.py
@@ -1,1 +1,41 @@
-# Breadth-First-Search (BFS) algorithm - version 1
+# Breadth-First-Search (BFS) - Recursive, where graph is a dict of key:list values.
+from typing import Any, Dict, Optional
+from copy import copy
+
+def breadth_first_search(graph: Dict, source, target, depth=1) -> Optional[Any]:
+    """Returns depth where target is first found, or None if not found."""
+    to_check = []
+
+    if source in graph:
+        for connection in graph[source]:
+            if connection == target:
+                return depth
+            else:
+                to_check.append(connection)
+        # If target wasn't found at this depth:
+        # - remove current node from graph (avoids cycles)
+        # - perform BFS on next depth
+        del graph[source]
+        for new_source in to_check:
+            print(f"      depth == {depth}")
+            new_bfs = breadth_first_search(graph, new_source, target, depth + 1)
+            if new_bfs is not None:
+                return new_bfs 
+    
+    # Target was not found
+    return None
+
+def test_bfs():
+    suite = [
+        ({'A': ['B']}, 'A', 'B', 1),
+        ({'A': ['B']}, 'C', 'B', None),
+        ({'A': ['B','C'],'C': ['D']}, 'A', 'D', 2),
+        ({'A': ['B','C'],'C': ['D']}, 'A', 'E', None),
+        ({'A': ['B','C'],'C': ['D']}, 'B', 'D', None),
+        ({'A': ['B','D'],'B': ['C','J'],'D': ['G','H'],'E': ['C'],'H': ['E']}, 'A', 'C', 2),
+        ({'A': ['B','D'],'B': ['C','J'],'D': ['G','H'],'E': ['C'],'H': ['E']}, 'D', 'C', 3),
+    ]
+    for graph, source, target, expected in suite:
+        # Make copy of each graph, since this BFS mutates the graph
+        graph = copy(graph)
+        assert breadth_first_search(graph, source, target) == expected
